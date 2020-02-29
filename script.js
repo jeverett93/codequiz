dcdf455c08cd40f1137527c9fd02932522ca0f10
Josh Everett <josheverett93@gmail.com>
2020-02-29 12:19:37 -0600

    put questions, answers, and correct answer in an object; made questions choices buttons; added buttons to page

--- script.js
+++ script.js
@@ -1,52 +1,87 @@
-var questions = document.getElementById("question-content");
-var choices = ["Migos", "THE DAB CORP.", "Rosa Parks", "MC Hammer"];
-// var correctAnswer = choices.indexOf();
-var userAnswer = ""; 
+var quizContent = document.getElementById("question-content");
+var questions = [
+    {
+        question: "Who invented the dab?",
+        potential_answers: ["Migos","THE DAB CORP.", "Rosa Parks", "MC Hammer"],
+        correct_answer: "Migos"
+    },
+
+    {
+        question: "What exactly was on fleek in its first known use?",
+        potential_answers: [
+            {
+                0: "an outfit"
+            },
+            {
+                1: "makeup"
+            },
+            {
+                2: "eyebrows"
+            },
+            {
+                3: "box braids"
+            },
+        ],
+        correct_answer: "eyebrows"
+    },
+]
 
 
 
-questions.innerText = "Who invented the dab?";
 
-for (var i = 0; i < choices.length; i++) {
+var currentQuestion = questions[0].question;
+var correctAnswer = questions[0].correct_answer;
+var userAnswer;
+
+
+
+for (var i = 0; i < questions[0].potential_answers.length; i++) {
     
-   var newP = document.createElement("p");
+   var newH = document.createElement("h1");
+
+    var newP = document.createElement("p");
 
    var btn = document.createElement("button");
 
+
+   newH.innerText = currentQuestion;
+
    
 //    Have each button be a choice in the array
-   btn.innerText = choices[i];
+   btn.innerText = questions[0].potential_answers[i];
 
    // give data attributes to question choices
      btn.setAttribute("data-index", i);
 
-     console.log(newP);
-
 
     // add event listener that, when clicked, calls on function that reveals whether or not answer is correct.
-     btn.addEventListener("click", seeResults())
+    //  btn.addEventListener("click", seeResults);
 
     
      newP.appendChild(btn);
 
-     questions.append(newP);
+     newH.appendChild(newP);
+
+     quizContent.append(newH);
 }
 
 
 // Function that tells user whether or not the answer is correct
 function seeResults() {
+
+    // when called by whichever button choice is selected, get the data index attribute of the button
     var selected = btn.getAttribute("data-index");
 
-    if(selected === choices.indexOf("Migos")) {
-        alert("Correct! MLK and Aretha Franklin smile upon you")
-    }
+    // if answer is Migos, alert or return correct
 
-    else (selected !== choices.indexOf("Migos")); {
-        alert("Wrong! Harriet Tubman weeps...")
-    };
+    
+    // if answer is anything other than Migos, alert or return wrong
 
-    console.log(seeResults())
-}
+
+   
+};
+
+console.log(seeResults())
 
 
 
