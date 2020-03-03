1b9000dbbbba6483d123bef84483690af3b0341c
Josh Everett <josheverett93@gmail.com>
2020-03-03 05:38:14 -0600

    disabled one of the navbar collapse feature

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 var score = 0;
 var highscore = localStorage.getItem("highscores");
 var startButton = document.getElementById("start-button");
+
+// starts quiz when begin is clicked in HTML
 startButton.addEventListener("click", startQuiz);
 var quizContainer = document.getElementById("quiz-container");
 var questionNum = 0;
 var timer;
 var quizTime = 60;
+if (quizTime <= 0) {
+    endQuiz();
+  };
 var timerEl = document.getElementById("timeClock");
 var scoreKeep = [];
 
+// defines quiz questions as an object to be called on later
 var questions = [
     {
         question: "Who invented the dab?",
@@ -38,7 +44,7 @@ var questions = [
     },
 ]
 
-
+// function that starts quiz which renders questions and starts timer
 function startQuiz() {
 
   //render first question
@@ -46,13 +52,20 @@ function startQuiz() {
   startTimer()
 }
 
+// timer function
 function startTimer() {
   timer = setInterval(()=>{
     quizTime --
     timerEl.innerText = quizTime
   },1000)
+
+  // if (quizTime <= 0) {
+  //   clearInterval(timer);
+  //   endQuiz();
+  // };
 }
 
+// function for questions that makes each answer choice a button as well as making the questions a header
 function renderQuestion() {
   // make a button for each answer
   var buttons = questions[questionNum].potential_answers.map(function (answer) {
@@ -65,21 +78,23 @@ function renderQuestion() {
   `
 }
 
+// function that increases user score as questions are answered correctly and takes time away if question answer is incorrect. 
 function answerQuestion(answer) {
   if (answer === questions[questionNum].correct_answer) {
     score ++;
-    alert("Correct!");
+    alert("Correct! And Black Jesus smiled!");
   } else {
-    // score --
     quizTime -= 10;
-    alert("Incorrect!");
+    alert("Incorrect! My ancestors weep...");
   }
 
 
-
+  // Makes sure each question continues to pop up dynamically.
   questionNum ++;
   console.log(questionNum)
   console.log(score)
+
+  // triggers end quiz function if user completes quiz question or time runs out
   if (questionNum === questions.length) {
     endQuiz()
   } else {
@@ -87,10 +102,10 @@ function answerQuestion(answer) {
   }
 }
 
+// Tells user what they're score is and allows them to input their initials.
 function endQuiz() {
-    // create input element and button
-    // Tells user what they're score is and allows them to input their initials.
 
+    // create input element and button as well as heading for score to be placed into.
     var inputName = document.createElement("input");
     var newBtn = document.createElement("button");
     var newHead = document.createElement("h4");
@@ -98,7 +113,10 @@ function endQuiz() {
     inputName.setAttribute("placeholder", "Enter your initials please");
     newBtn.innerText = "Submit";
     newHead.innerHTML = "Your score is " + score;
+
+    // newBtn.addEventListener("click", addStorage)
     
+    // puts this all on one page
     quizContainer.innerHTML = "";
     quizContainer.appendChild(newHead
       );
@@ -106,13 +124,14 @@ function endQuiz() {
     quizContainer.appendChild(newBtn);
     
 
-    scoreKeep.push(score)
+    scoreKeep.push(score);
     localStorage.setItem("highscores", scoreKeep);
     
   
-    console.log(scoreKeep)
-    console.log(localStorage)
+    console.log(scoreKeep);
+    console.log(localStorage);
     
+    // stops timer when quiz ends
   clearInterval(timer)
 
 
